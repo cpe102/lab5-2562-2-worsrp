200ca7805ddba5dd5d497cf178aaf8c4a1b4d73c
Sirapop Para <worwee07@MacBook-Pro.local>
2020-01-07 14:59:28 +0700

    editted

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,22 +2,32 @@
 using namespace std;
 
 int main(){
-	int N;
+	int N,i=0;
 	cout << "Enter the number of student: ";
 	cin >> N;
 	
 	string name[N];
 	int age[N];
-	cout << "Name of student = ";
-	cout << "Age of student = ";
+	while(i<N){
+		cout << "Name of student " << i+1 << " =";
+		cin >> name[i];
+		cout << "Age of student " << i+1 <<  " =";
+		cin >> age[i];
+		i++;
+	}
 	
-	int key;
+	int key,count=0;
 	cout << "--------------------------------------\n";
 	cout << "Enter an age to search: ";
 	cin >> key;
 	cout << "--------------------------------------\n";
-	
-
+	while (count<N)
+	{
+		if(key==age[count]){
+			cout << name[count] << "\n";
+		}
+		count++;
+	}
 	
 	cout << "--------------------------------------\n";
 	
